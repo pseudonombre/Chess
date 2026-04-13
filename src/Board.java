f63bea0a82f961e97bacf122ff8398efb31f6cd5
Anderson <andersoncd24@gcc.edu>
2026-04-13 18:15:44 -0400

    Commented Board.java, added constructors and makeMove function

--- src/Board.java
+++ src/Board.java
@@ -1,5 +1,24 @@
+/**
+ * Represents a board. holds pieces and manages whether movement is possible
+ */
 public class Board {
+    /**
+     * The 2D array holding all the pieces
+     */
     private Piece[][] pieces = new Piece[8][8];
+
+    /**
+     * Creates a new board with the standard chess starting position
+     */
+    public Board(){
+    }
+
+    /**
+     * Creates a new board with the same piece configuration
+     * @param c_pieces the configuration to put the new board in
+     */
+    public Board(Piece[][] c_pieces){
+    }
 
     /**
      * @return a string representation of the board
@@ -21,11 +40,19 @@ public class Board {
      *     getting if the piece would be able to move there on an empty board (call Piece.isPossible),
      *     getting the squares that must be empty (call Piece.isPossible) and checking if they are empty
      *     and the moving player's king is not in check
+     * @param move the move to check the possibility of
      * @param from where the move is from
-     * @param to where the move is to
      * @return true if the move is legal according to the rules of chess
      */
-    public boolean isPossible(int[] from, int[] to) {
+    public boolean isPossible(Move move, int[] from) {
         return false;
+    }
+
+    /**
+     * Makes the given move after checking if it is possible.
+     * @param move the move to make
+     * @param from where the move is from
+     */
+    public void makeMove(Move move, int[] from) {
     }
 }
